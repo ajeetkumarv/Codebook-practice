8262c323b1ae7f95e24d074823d31415a00b87d2
ajeetkumar vishwakarma <ajeetkumar.vishwakarma@here.com>
2022-01-16 18:01:09 +0530

    1 Junit class, 1 main class

---
+++ java/src/main/java/code/MainTestClass.java
@@ -0,0 +1,13 @@
+package code;
+
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
+public class MainTestClass {
+
+    public static void main(String[] args) {
+        System.out.println("This is a learning code project");
+    }
+
+}
---
+++ java/src/test/java/code/MainTestClass.java
@@ -0,0 +1,12 @@
+package code;
+
+import org.junit.jupiter.api.Test;
+
+public class MainTestClass {
+
+    @Test
+    public void testMethod() {
+        System.out.println("This is a test method for JUnit 5");
+    }
+
+}
