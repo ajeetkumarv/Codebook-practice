0dfe89b9b39dd45373ec20956e6225f7b1516984
Ajeet <ajeetkumarv@outlook.com>
2023-08-10 08:29:32 -0400

    algods TargetSum

---
+++ java/src/main/java/code/algods/arrays/TargetSum.java
@@ -0,0 +1,53 @@
+package code.algods.arrays;
+
+import java.util.*;
+
+public class TargetSum {
+
+    public static void main(String[] args) {
+
+        int[] withDuplicateNumber = {5, 5, 3, 2};
+        System.out.println(Arrays.toString(findPairWithTargetSum(withDuplicateNumber, 10)));
+
+        System.out.println(Arrays.toString(findPairWithTargetSumSortedArrayApproach(withDuplicateNumber, 10)));
+    }
+
+    private static int[] findPairWithTargetSum(int[] array, int targetSum) {
+
+        Set<Integer> visited = new HashSet<>();
+
+        for(int current: array) {
+            int potentialMatch = targetSum - current;
+            if (visited.contains(potentialMatch)) {
+                return new int[] {current, potentialMatch};
+            } else {
+                visited.add(current);
+            }
+        }
+
+        return new int[0];
+    }
+
+    private static Integer[][] findPairWithTargetSumSortedArrayApproach(int[] array, int targetSum) {
+        Arrays.sort(array);
+
+        List<Integer[]> pairs = new ArrayList<>();
+
+        for (int i=0, j =array.length - 1; i < j;) {
+            int sum = array[i] + array[j];
+
+            if (sum == targetSum) {
+                pairs.add(new Integer[] {array[i], array[j]});
+                i++;
+                j--;
+            } else if ( sum < targetSum) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+
+        return pairs.toArray(new Integer[0][]);
+    }
+
+}
