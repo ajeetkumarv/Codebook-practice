63457bf112672db4f0e25a191182324dac1ec2fb
ajeetkumarv <ajeetkumarv@outlook.com>
2023-08-15 21:45:24 +0200

    algods OneEditMatchWords /buggy

---
+++ java/src/main/java/code/algods/string/OneEditMatchWords.java
@@ -0,0 +1,54 @@
+package code.algods.string;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class OneEditMatchWords {
+
+    private static Set<Integer> expectedSizes = Set.of(1, 0);
+    public static void main(String[] args) {
+
+        System.out.println(oneEditPossibleToMatch("hello", "olleg"));
+        System.out.println(oneEditPossibleToMatch("hello", "hollo"));
+        //System.out.println(oneEditPossibleToMatch("hello", "hllo"));
+        //System.out.println(oneEditPossibleToMatch("bb", "a"));
+        //System.out.println(oneEditPossibleToMatch("hello", "heloos"));
+        System.out.println(oneEditPossibleToMatch("hello", "ellos"));
+    }
+
+    private static boolean oneEditPossibleToMatch(String stringOne, String stringTwo) {
+        // This is buggy, doesnot work for "hello", "ellos"), bcdefghijklmnopqrstuvwxyz", abdefghijklmnopqrstuvwxyz
+        int sizeDiff = Math.abs(stringOne.length() - stringTwo.length());
+
+        if (sizeDiff > 1) {
+            return false;
+        }
+
+        if (sizeDiff <= 1) {
+            List<Character> oneChars = stringOne.codePoints().mapToObj(c -> (char) c).collect(Collectors.toList());
+            List<Character> twoChars = stringTwo.codePoints().mapToObj(c -> (char) c).collect(Collectors.toList());
+
+            List<Character> oneCharBkp = new ArrayList<>(oneChars);
+
+            if (Math.abs(oneChars.size() -  twoChars.size()) > 1) {
+                return false;
+            }
+
+            remove(oneChars, twoChars);
+            remove(twoChars, oneCharBkp);
+
+            return expectedSizes.contains(oneChars.size()) && expectedSizes.contains(twoChars.size());
+        }
+
+        return false;
+    }
+
+    private static void remove(List<Character> removeFromIt, List<Character> removeThese) {
+        for (Character c: removeThese) {
+            removeFromIt.remove(c);
+        }
+    }
+}
