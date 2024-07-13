63a924500de47a1afbf24a504a8a83af2d3beeca
Ajeet <ajeetkumarv@outlook.com>
2024-07-13 13:50:21 +0200

    Practice code setup

--- java/src/test/java/practice/stream/MovieTest.java
+++ java/src/test/java/practice/stream/MovieTest.java
@@ -13,16 +13,7 @@ class MovieTest {
 
 	@BeforeAll
 	public void setup() {
-		Movie movie1 = new Movie(10, "Sikandar", Movie.Genre.ACTION, "Hindi",
-				List.of("Summy Deol", "Dharmendra paji", "Juhi chawla"),
-				List.of("English"),
-				Duration.ofMinutes(120));
-
-		Movie movie2 = new Movie(0, "Sikandar", Movie.Genre.ACTION, "Hindi",
-				List.of("Summy Deol", "Dharmendra paji", "Juhi chawla"),
-				List.of("English"),
-				Duration.ofMinutes(120));
-
+		populateMovies();
 	}
 
 	@Test
@@ -75,5 +66,42 @@ class MovieTest {
 
 	@Test
 	public void testGenreAndCommaSeparatedTitles() {
+	}
+
+	private void populateMovies() {
+		Movie movie1 = new Movie(10, "Sikandar", Movie.Genre.ACTION, "Hindi",
+				List.of("Summy Deol", "Dharmendra paji", "Juhi chawla"),
+				List.of("English"),
+				Duration.ofMinutes(120));
+
+		Movie movie2 = new Movie(129, "The 400 Blows", Movie.Genre.THRILLER, "English",
+				List.of("Jean Pierre", "Albert Remy", "Claire Maurier"),
+				List.of("Dutch", "Spanish"),
+				Duration.ofMinutes(100));
+
+		Movie movie3 = new Movie(0, "Paradise", Movie.Genre.ACTION, "German",
+				List.of("Daniel", "tina"),
+				List.of("Dutch", "Spanish"),
+				Duration.ofMinutes(135));
+
+		Movie movie4 = new Movie(1, "Godforsaken", Movie.Genre.HORROR, "Dutch",
+				List.of("Egbert Jan Weeber", "Tygo", "Angela", "Schijf", "Franken", "Krake"),
+				List.of(),
+				Duration.ofMinutes(70));
+
+		Movie movie5 = new Movie(55, "The 400 Blows", Movie.Genre.ACTION, "Telugu",
+				List.of("NT Rao", "Savitri"),
+				List.of("Hindi", "Tamil", "English", "Malayalam", "Kannad", "Odia", "Asammese", "Punjabi"),
+				Duration.ofMinutes(90));
+
+		Movie movie6 = new Movie(20, "Drishyam", Movie.Genre.DRAMA, "Hindi",
+				List.of("Ajay devgun"),
+				List.of(),
+				Duration.ofMinutes(110));
+
+		Movie movie7 = new Movie(20, "TBahubali", Movie.Genre.ACTION, "Hindi",
+				List.of("Samantha"),
+				List.of("Telugu", "Tamil", "Malayalam"),
+				Duration.ofMinutes(150));
 	}
 }
